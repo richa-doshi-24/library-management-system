dcebc5ca2dad56748cc425b68433b532430a1b1c
Richa Doshi <richadoshi@Richas-MacBook-Air-2.local>
2023-11-04 15:46:01 -0400

    Database connection, UI integration and ui components

---
+++ frontend/library-app/src/layouts/HomePage/components/HomePage.tsx
@@ -0,0 +1,14 @@
+import { Carousel } from "./Carousel";
+import { ExploreTopBooks } from "./ExploreTopBooks";
+
+
+export const HomePage = () => {
+    return (
+        <>
+            <ExploreTopBooks/>
+            <Carousel/>
+            {/* <Heros/> */}
+            {/* <LibraryServices/> */}
+        </>
+    );
+}
